1b85d527eabb2de7cb9b31d25f54b76304d96b4e
Noella Jarega <noellajarega@gmail.com>
2023-06-26 14:14:05 +0300

    Defer

--- 0x16-api_advanced/README.md
+++
@@ -1,1 +0,0 @@
-*Python requests package exercics*
--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-#Fix error with strace command and fix it and automate using Puppet
-
-file { '/var/www/html/wp-includes/class-wp-locale.phpp':
-  ensure => file,
-  source => '/var/www/html/wp-includes/class-wp-locale.php',
-}
